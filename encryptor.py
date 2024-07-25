91e5adad789c159f0ab97ac74a50fa997093da1c
Greg P <88697977+zvxvx@users.noreply.github.com>
2024-07-25 15:35:37 -0700

    Complete scaffold for cla

---
+++ encryptor.py
@@ -0,0 +1,87 @@
+def main():
+  menu()
+
+def menu():
+   choice = input("""
+   Select the type of encryption you wish to perform: 
+   1) Symmetric Encryption
+   2) Asymmetric Encryption
+   """)
+   c = int(choice)
+
+   if c == 1:
+    sym_menu()
+   elif c == 2:
+    asym_menu()
+
+def sym_menu():
+  sym_enc = input("""
+    Select the type of cipher you wish to use:
+    1) Traditional Cipher
+    2) Modern Cipher
+    """)
+  s = int(sym_enc)
+
+  if s == 1:
+    trad_ciphers()
+  elif s == 2:
+    mod_ciphers()
+
+def asym_menu():
+  asym_enc = input("""
+  Select the type of Asymmetric cipher you wish to use:
+  1) RSA (Rivist-Shamir-Adleman)
+  """)
+  a = int(asym_enc)
+
+  if a == 1:
+    rsa()
+
+def rsa():
+  print("This is the RSA cipher")
+
+def trad_ciphers():
+    trad_ciph = input("""
+    Select the type of traditional cipher you wish to use:
+    1) Caeser Cipher
+    2) Autokey Cipher
+    3) Rail Fence Cipher
+    """)
+    t = int(trad_ciph)
+
+    if t == 1:
+      caeser()
+    elif t == 2:
+      autokey()
+    elif t == 3:
+      rail()
+
+def caeser():
+  print("This is the caeser cipher")
+
+def autokey():
+  print("This is the autokey cipher")
+
+def rail():
+  print("This is the rail fence cipher")
+
+def mod_ciphers():
+    mod_ciph = input("""
+    Select the type of modern cipher you wish to use:
+    1) AES (Advanced Encryption Standard)
+    2) DES (Data Encryption Standard)
+    """)
+    m = int(mod_ciph)
+
+    if m == 1:
+      aes()
+    elif m == 2:
+      des()
+
+def aes():
+  print("This is the AES cipher")
+
+def des():
+  print("This is the DES cipher")
+
+main()
