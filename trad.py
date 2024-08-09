b02af25bba420ca8ac2d45a7dc0e4506201c76ef
Jake Lucas <156277910+JacobLucas02@users.noreply.github.com>
2024-08-09 13:45:43 -0700

    Update trad.py
    Fixed encrypt and decrypt rail fence functions which were giving the wrong outputs.

--- trad.py
+++ trad.py
@@ -89,57 +89,83 @@ def autokey():
     autokey()
 
 def encrypt_rail_fence(plaintext, num_rails):
-    rail = [''] * num_rails
-    direction = None
-    row = 0
-    for char in plaintext:
-        rail[row] += char
-        if row == 0:
-            direction = 1
-        elif row == num_rails - 1:
-            direction = -1
-        row += direction
+    rail = [['' for _ in range(len(plaintext))] for _ in range(num_rails)]
+
+    dir_down = False
+    row, col = 0, 0
+
+    for i in range(len(plaintext)):
+        if (row == 0) or (row == num_rails - 1):
+            dir_down = not dir_down
+
+        rail[row][col] = plaintext[i]
+        col += 1
+
+        if dir_down:
+            row += 1
+        else:
+            row -= 1
+
+    result = []
+    for i in range(num_rails):
+        for j in range(len(plaintext)):
+            if rail[i][j] != '':
+                result.append(rail[i][j])
+
+    encrypted_message = ''.join(result)
+
     return f"""
     ==========
-    Your encrypted message is: {''.join(rail)}.
-    Your key is {num_rails}. 
+    Your encrypted message is: {encrypted_message}.
+    Your key is {num_rails}.
     Please keep this safe!
     ==========
     """
 
 def decrypt_rail_fence(ciphertext, num_rails):
-    rail_len = len(ciphertext)
-    rail = [['\n' for _ in range(rail_len)] for _ in range(num_rails)]
-    
-    direction = None
+    rail = [['\n' for i in range(len(ciphertext))] for j in range(num_rails)]
+
+    dir_down = None
     row, col = 0, 0
 
-    for _ in range(rail_len):
+    for i in range(len(ciphertext)):
+        if row == 0:
+            dir_down = True
+        if row == num_rails - 1:
+            dir_down = False
+
         rail[row][col] = '*'
         col += 1
-        if row == 0:
-            direction = 1
-        elif row == num_rails - 1:
-            direction = -1
-        row += direction
-    
+
+        if dir_down:
+            row += 1
+        else:
+            row -= 1
+
     index = 0
     for i in range(num_rails):
-        for j in range(rail_len):
-            if rail[i][j] == '*' and index < rail_len:
+        for j in range(len(ciphertext)):
+            if (rail[i][j] == '*') and (index < len(ciphertext)):
                 rail[i][j] = ciphertext[index]
                 index += 1
-    
+
     result = []
     row, col = 0, 0
-    for _ in range(rail_len):
-        result.append(rail[row][col])
-        col += 1
+    for i in range(len(ciphertext)):
+
         if row == 0:
-            direction = 1
-        elif row == num_rails - 1:
-            direction = -1
-        row += direction
+            dir_down = True
+        if row == num_rails - 1:
+            dir_down = False
+
+        if rail[row][col] != '*':
+            result.append(rail[row][col])
+            col += 1
+
+        if dir_down:
+            row += 1
+        else:
+            row -= 1
 
     return f"""
     ==========
